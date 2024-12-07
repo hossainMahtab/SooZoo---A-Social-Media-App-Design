6bdc4e93e89416eb5c7cb39c5cf7042ec4beef11
Mahtab-Hossain-005 <2018-1-68-005@std.ewubd.edu>
2024-12-07 18:37:24 +0200

    jhamela

--- my-app/src/context/AuthContext.js
+++ my-app/src/context/AuthContext.js
@@ -3,9 +3,15 @@ import React, { useEffect, useState } from "react";
 export const AuthContext = React.createContext();
 
 const AuthContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user") || null)
-  );
+  // const [currentUser, setCurrentUser] = useState(
+  //   JSON.parse(localStorage.getItem("user") || null)
+  // );
+  const [currentUser, setCurrentUser] = useState({
+    id: 7,
+    username: "Albert",
+    profilePic:
+      "https://images.pexels.com/photos/1509684/pexels-photo-1509684.jpeg?auto=compress&cs=tinysrgb&w=600",
+  });
 
   const login = () => {
     setCurrentUser({
