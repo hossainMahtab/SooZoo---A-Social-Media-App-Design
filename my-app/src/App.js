0472f64e1afca0cbbb06c1706cfef8a843a58023
Mahtab-Hossain-005 <2018-1-68-005@std.ewubd.edu>
2024-12-07 15:20:06 +0200

    long update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,94 +1,95 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 import {
   createBrowserRouter,
   Navigate,
   Outlet,
   RouterProvider,
-
 } from "react-router-dom";
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import Navbar from './components/Navbar';
-import Leftbar from './components/Leftbar';
-import Rightbar from './components/Rightbar'
-import Home from './pages/home/Home'
-import Profile from './pages/profile/Profile';
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Navbar from "./components/Navbar";
+import Leftbar from "./components/Leftbar";
+import Rightbar from "./components/Rightbar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
-import { useContext } from 'react';
-import { AuthContext } from './context/AuthContext';
-import Menubar from './components/Menubar';
-
-
-
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import Menubar from "./components/Menubar";
 
 const App = () => {
+  const { currentUser } = useContext(AuthContext);
 
-  const { currentUser } = useContext(AuthContext)
-
-  const [menuClick, setMenuClick] = useState(false)
+  const [menuClick, setMenuClick] = useState(false);
 
   const handleClickMenu = () => {
-    setMenuClick(!menuClick)
-  }
-
-
+    setMenuClick(!menuClick);
+  };
 
   const Layout = () => {
     return (
       <div>
         <Navbar onMenu={handleClickMenu} />
-        {menuClick ? <div className=' col-span-12' ><Menubar /></div> : <Outlet />
-        }
-
-
+        {menuClick ? (
+          <div className=" col-span-12">
+            <Menubar />
+          </div>
+        ) : (
+          <Outlet />
+        )}
       </div>
-    )
+    );
+  };
 
-  }
-
-  const ProtectedRoute = ({ children }) => {
-    if (currentUser) {
-      return children
-    } else {
-      return <Navigate to='/login' />
-    }
-  }
+  // const ProtectedRoute = ({ children }) => {
+  //   if (currentUser) {
+  //     return children
+  //   } else {
+  //     return <Navigate to='/login' />
+  //   }
+  // }
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <ProtectedRoute>
+      element: (
+        //  <ProtectedRoute>
+        // </ProtectedRoute>,
         <Layout />
-      </ProtectedRoute>,
+      ),
       children: [
         {
-
           path: "/",
-          element:
-            <div className='grid grid-cols-12'>
-              <div className=' col-span-2 md:hidden'><Menubar /></div>
+          element: (
+            <div className="grid grid-cols-12">
+              <div className=" col-span-2 md:hidden">
+                <Menubar />
+              </div>
 
-              <div className=' hidden md:block md:col-span-3 lg:col-span-2'><Leftbar /></div>
-              <div className=' col-span-10  md:col-span-9 lg:col-span-7' ><Home /></div>
-              <div className=' hidden lg:block lg:col-span-3'> <Rightbar /></div>
-
+              <div className=" hidden md:block md:col-span-3 lg:col-span-2">
+                <Leftbar />
+              </div>
+              <div className=" col-span-10  md:col-span-9 lg:col-span-7">
+                <Home />
+              </div>
+              <div className=" hidden lg:block lg:col-span-3">
+                {" "}
+                <Rightbar />
+              </div>
             </div>
-
+          ),
         },
         {
           path: "/profile/:id",
-          element:
+          element: (
             <div>
               <Profile />
             </div>
-
-        }
-
-      ]
-
+          ),
+        },
+      ],
     },
-
 
     {
       path: "/login",
@@ -106,20 +107,15 @@ const App = () => {
         </div>
       ),
     },
-
-
   ]);
 
   return (
-
     <div>
       <React.StrictMode>
         <RouterProvider router={router} />
       </React.StrictMode>
     </div>
+  );
+};
 
-
-  )
-}
-
-export default App
+export default App;
--- my-app/src/components/SharePost.js
+++ my-app/src/components/SharePost.js
@@ -1,38 +1,59 @@
-import React from 'react'
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const SharePost = () => {
-    const { currentUser } = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
   return (
-    <div className='w-full h-[160px] p-2'>
-      <div className='w-full h-full bg-white px-2 py-4 rounded'> 
-        <div className='flex w-full h-[70%] gap-2 p-2'> 
-            <img className='w-[20px] h-[20px] md:w-[25px] md:h-[25px] rounded-full object-cover' src={currentUser.profilePic} alt="" />
-            <textarea className=' w-[95%] resize-none border-none outline-none px-2 overflow-auto scrollbar-hide' placeholder={`What's on your mind ${currentUser.username}?`}/>
+    <div className="w-full h-[160px] p-2">
+      <div className="w-full h-full bg-white px-2 py-4 rounded">
+        <div className="flex w-full h-[70%] gap-2 p-2">
+          <img
+            className="w-[20px] h-[20px] md:w-[25px] md:h-[25px] rounded-full object-cover"
+            src={currentUser?.profilePic}
+            alt=""
+          />
+          <textarea
+            className=" w-[95%] resize-none border-none outline-none px-2 overflow-auto scrollbar-hide"
+            placeholder={`What's on your mind ${currentUser.username}?`}
+          />
         </div>
-        <div className='flex items-center h-[30%] w-full '> 
-            <div className='w-[70%] lg:w-[60%] flex justify-between h-full p-2'> 
-                <div className='flex items-center  gap-2'>
-                    <img className='w-[30px] h-[30px]' src='https://freeiconshop.com/wp-content/uploads/edd/image-outline-filled.png' alt=''/>
-                    <span className='hidden md:block '>Add Image</span>
-                </div>
-                <div className='flex items-center gap-2'>
-                    <img className='w-[30px] h-[30px]' src='https://www.iconpacks.net/icons/2/free-location-icon-2955-thumb.png' alt=''/>
-                    <span className='hidden md:block '>Add Place</span>
-                </div>
-                <div className='flex items-center gap-2'>
-                    <img className='w-[30px] h-[30px]' src='https://icon-library.com/images/refer-a-friend-icon-png/refer-a-friend-icon-png-19.jpg' alt=''/>
-                    <span className='hidden md:block '>Tag Friends</span>
-                </div>               
+        <div className="flex items-center h-[30%] w-full ">
+          <div className="w-[70%] lg:w-[60%] flex justify-between h-full p-2">
+            <div className="flex items-center  gap-2">
+              <img
+                className="w-[30px] h-[30px]"
+                src="https://freeiconshop.com/wp-content/uploads/edd/image-outline-filled.png"
+                alt=""
+              />
+              <span className="hidden md:block ">Add Image</span>
             </div>
-            <div className='w-[30%] lg:w-[40%] flex items-center justify-end h-full p-2'> 
-                <button className='px-2 py-1 rounded bg-blue-600 text-white'>Post</button>
+            <div className="flex items-center gap-2">
+              <img
+                className="w-[30px] h-[30px]"
+                src="https://www.iconpacks.net/icons/2/free-location-icon-2955-thumb.png"
+                alt=""
+              />
+              <span className="hidden md:block ">Add Place</span>
             </div>
+            <div className="flex items-center gap-2">
+              <img
+                className="w-[30px] h-[30px]"
+                src="https://icon-library.com/images/refer-a-friend-icon-png/refer-a-friend-icon-png-19.jpg"
+                alt=""
+              />
+              <span className="hidden md:block ">Tag Friends</span>
+            </div>
+          </div>
+          <div className="w-[30%] lg:w-[40%] flex items-center justify-end h-full p-2">
+            <button className="px-2 py-1 rounded bg-blue-600 text-white">
+              Post
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SharePost
+export default SharePost;
